389962433e664a6fbde7c309f9e349dc58e53b14
fusaa~ <ffsabbagh@gmail.com>
2022-06-26 19:20:56 +0100

    minors

--- house.py
+++ house.py
@@ -26,8 +26,8 @@ data['price_sqft'] = data['price'] / data['sqft_lot']
 
 
 
-# --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-# Data Overview
+# # --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+# # Data Overview
 
 # 1.1 SIDEBAR - Data Overview filters
 f_attributes = st.sidebar.multiselect('Enter columns', data.columns) # criar campo pra especificar filtro
@@ -44,40 +44,51 @@ elif (f_zipcode == [] ) & (f_attributes != []):
     data = data.loc[:, f_attributes]
 else:
     data = data.copy()
+#
+# st.dataframe(data.head())
+#
+# # 2 MAIN CONTENT - 2nd section, build average/count table
+# df1 = data[['id', 'zipcode']].groupby('zipcode').count().reset_index()
+# df2 = data[['price', 'zipcode']].groupby('zipcode').mean().reset_index()
+# df3 = data[['sqft_living', 'zipcode']].groupby('zipcode').mean().reset_index()
+# df4 = data[['price_sqft', 'zipcode']].groupby('zipcode').mean().reset_index()
+# m1 = pd.merge(df1,df2, on='zipcode', how='inner')
+# m2 = pd.merge(m1,df3, on='zipcode', how='inner')
+# df = pd.merge(m2,df4, on='zipcode', how='inner')
+#
+# df.columns = ['Zipcode','Total Houses', 'Price','Square feet -living',
+#               'Price/sqft']
+#
+# c1, c2 = st.columns(( 1,1 ))  # 2 columns
+# c1.header('Average Values - S')  # 1st column
+# c1.dataframe(df, height = 600)  # 1st column
+#
+# # 3 MAIN CONTENT - 3rd section, descriptive stats
+# num_attributes = data.select_dtypes(include=['int64', 'float64'])
+# mean = pd.DataFrame(num_attributes.apply( np.mean ))
+# median = pd.DataFrame(num_attributes.apply( np.median ))
+# std = pd.DataFrame(num_attributes.apply( np.std ))
+#
+# max_ = pd.DataFrame( num_attributes.apply ( np.max ))
+# min_ = pd.DataFrame( num_attributes.apply ( np.min ))
+#
+# df1 = pd.concat([max_,min_, mean, median, std], axis = 1).reset_index()
+# df1.columns = ['Attributes', 'Max', 'Min', 'Mean', 'Median', 'Std']
+#
+# c2.header('Descriptive Statistics')
+# c2.dataframe( df1, height = 800)
+#
+##################
 
-st.dataframe(data.head())
+# with st.expander("See explanation"):
 
-# 2 MAIN CONTENT - 2nd section, build average/count table
-df1 = data[['id', 'zipcode']].groupby('zipcode').count().reset_index()
-df2 = data[['price', 'zipcode']].groupby('zipcode').mean().reset_index()
-df3 = data[['sqft_living', 'zipcode']].groupby('zipcode').mean().reset_index()
-df4 = data[['price_sqft', 'zipcode']].groupby('zipcode').mean().reset_index()
-m1 = pd.merge(df1,df2, on='zipcode', how='inner')
-m2 = pd.merge(m1,df3, on='zipcode', how='inner')
-df = pd.merge(m2,df4, on='zipcode', how='inner')
+st.markdown('Insights on real estate for purchase and resaleInsights on real estate for purchase and resale')
 
-df.columns = ['Zipcode','Total Houses', 'Price','Square feet -living',
-              'Price/sqft']
-
-c1, c2 = st.columns(( 1,1 ))  # 2 columns
-c1.header('Average Values - S')  # 1st column
-c1.dataframe(df, height = 600)  # 1st column
-
-# 3 MAIN CONTENT - 3rd section, descriptive stats
-num_attributes = data.select_dtypes(include=['int64', 'float64'])
-mean = pd.DataFrame(num_attributes.apply( np.mean ))
-median = pd.DataFrame(num_attributes.apply( np.median ))
-std = pd.DataFrame(num_attributes.apply( np.std ))
-
-max_ = pd.DataFrame( num_attributes.apply ( np.max ))
-min_ = pd.DataFrame( num_attributes.apply ( np.min ))
-
-df1 = pd.concat([max_,min_, mean, median, std], axis = 1).reset_index()
-df1.columns = ['Attributes', 'Max', 'Min', 'Mean', 'Median', 'Std']
-
-c2.header('Descriptive Statistics')
-c2.dataframe( df1, height = 800)
-
+st.write("""
+     The chart above shows some numbers I picked for you.
+     I rolled actual dice for these, so they're *guaranteed* to
+     be random.
+ """)
 
 # 4 MAIN CONTENT - 4th section, portfolio density
 st.title('Region Overview')
@@ -94,9 +105,9 @@ density_map = folium.Map(location = [data['lat'].mean(),
                          default_zoom_start=15)
 
 marker_cluster = MarkerCluster().add_to(density_map)  # start class for pinmarks
-for name, row in df.iterrows():
+for name, row in data.iterrows():
     folium.Marker( [row['lat'], row['long'] ],\
-                   popup='Sold R${0} on: {1}. Features: {2} sqft, {3} bedrooms, {4} bathrooms, year built: {5}'.format( row['price'],\
+                   popup='Sold USD{0} on: {1}. Features: {2} sqft, {3} bedrooms, {4} bathrooms, year built: {5}'.format( row['price'],\
                                      row['date'],
                                      row['sqft_living'],
                                      row['bedrooms'],
@@ -111,7 +122,7 @@ c2.header('Price Density')
 
 df = data[['price','zipcode']].groupby('zipcode').mean().reset_index()
 df.columns = ['ZIP', 'PRICE']
-df = df.sample(11)  # limited for faster testing
+# df = df.sample(11)  # limited for faster testing
 
 geofile = geofile[geofile['ZIP'].isin( df['ZIP'].tolist() )] # filtering dataset with info in sample list
 
@@ -146,87 +157,88 @@ max_year_built = int( data['yr_built'].max() )
 
 st.sidebar.subheader('Select Max Year Built')
 f_year_built = st.sidebar.slider('Yr built', min_year_built,max_year_built,min_year_built)
+data = data.loc[data['yr_built'] < f_year_built]
 
-# 6.2 - Main Content
-st.header('Average Price vs Year Built')
+# # 6.2 - Main Content
+# st.header('Average Price vs Year Built')
+#
+# df = data.loc[data['yr_built'] < f_year_built]
+# df = df[['yr_built', 'price']].groupby('yr_built').mean().reset_index()
+#
+# fig = px.line(df, x='yr_built', y='price')
+# st.plotly_chart(fig, use_container_width=True)
 
-df = data.loc[data['yr_built'] < f_year_built]
-df = df[['yr_built', 'price']].groupby('yr_built').mean().reset_index()
-
-fig = px.line(df, x='yr_built', y='price')
-st.plotly_chart(fig, use_container_width=True)
-
-# 7 AVG price per day
-# 7.1  Sidebar
-st.sidebar.header("Max. date for Average Day Price")
-
-# 7.2 - MAIN SCREEN
-st.header('Avg Price per day')
-
-min_date = datetime.strptime( data['date'].min(), '%Y-%m-%d' )
-max_date = datetime.strptime( data['date'].max(), '%Y-%m-%d' )
-f_date = st.sidebar.slider('Date', min_date, max_date, min_date)
-
-data['date'] = pd.to_datetime(data['date'], format='%Y-%m-%d')
-df = data.loc[data['date'] < f_date]
-df = df[['date', 'price']].groupby('date').mean().reset_index()
-
-fig = px.line(df, x='date', y='price')
-st.plotly_chart(fig, use_container_width=True)
-
-#  Section - By Attributes
-# 8 Sidebars Bedroom
-st.sidebar.title('Attributes Options')
-
-f_bedrooms = st.sidebar.selectbox('Max # bedrooms:',
-                                  sorted( set(data['bedrooms'].unique())))
-
-# 9 Sidebar Bathroom
-f_bathrooms = st.sidebar.selectbox('Max # bathrooms:',
-                                   sorted( set(data['bathrooms'].unique())))
-
-#  8 and 9  Main window Bedroom and bathroom
-c1, c2 = st.columns( 2 )
-
-
-c1.header('Bedrooms')
-df = data[data['bedrooms'] < f_bedrooms]
-fig = px.histogram(df, x='bedrooms', nbins =19)
-c1.plotly_chart(fig, use_container_width=True)
-
-c2.header('Bathrooms')
-df = data[data['bathrooms'] < f_bathrooms]
-fig = px.histogram(df, x = 'bathrooms', nbins =19)
-c2.plotly_chart(fig, use_container_width=True)
-
-# Houses floor and Waterview
-# 10 - Sidebar Max number of floors
-f_floors = st.sidebar.selectbox('Max number of floor',
-                                   sorted( set(data['floors'].unique())))
-
-# 11 - Sidebar filter water view
-f_waterview = st.sidebar.checkbox( 'Only Houses with Water View')
-
-
-# 10 and 11 Main window:
-c1,c2 = st.columns(2)
-
-c1.title('Houses per floor')
-df = data[data['floors'] < f_floors]
-fig = px.histogram(df, x = 'floors', nbins =19)
-c1.plotly_chart(fig, use_container_width=True)
-
-
-c2.title('Waterview')
-if f_waterview:
-    df = data[data['waterfront'] == 1]
-else:
-    df= data.copy()
-
-
-fig = px.histogram(df, x='waterfront', nbins=10)
-c2.plotly_chart(fig, use_container_width=True)
-
-
-
+# # 7 AVG price per day
+# # 7.1  Sidebar
+# st.sidebar.header("Max. date for Average Day Price")
+#
+# # 7.2 - MAIN SCREEN
+# st.header('Avg Price per day')
+#
+# min_date = datetime.strptime( data['date'].min(), '%Y-%m-%d' )
+# max_date = datetime.strptime( data['date'].max(), '%Y-%m-%d' )
+# f_date = st.sidebar.slider('Date', min_date, max_date, min_date)
+#
+# data['date'] = pd.to_datetime(data['date'], format='%Y-%m-%d')
+# df = data.loc[data['date'] < f_date]
+# df = df[['date', 'price']].groupby('date').mean().reset_index()
+#
+# fig = px.line(df, x='date', y='price')
+# st.plotly_chart(fig, use_container_width=True)
+#
+# #  Section - By Attributes
+# # 8 Sidebars Bedroom
+# st.sidebar.title('Attributes Options')
+#
+# f_bedrooms = st.sidebar.selectbox('Max # bedrooms:',
+#                                   sorted( set(data['bedrooms'].unique())))
+#
+# # 9 Sidebar Bathroom
+# f_bathrooms = st.sidebar.selectbox('Max # bathrooms:',
+#                                    sorted( set(data['bathrooms'].unique())))
+#
+# #  8 and 9  Main window Bedroom and bathroom
+# c1, c2 = st.columns( 2 )
+#
+#
+# c1.header('Bedrooms')
+# df = data[data['bedrooms'] < f_bedrooms]
+# fig = px.histogram(df, x='bedrooms', nbins =19)
+# c1.plotly_chart(fig, use_container_width=True)
+#
+# c2.header('Bathrooms')
+# df = data[data['bathrooms'] < f_bathrooms]
+# fig = px.histogram(df, x = 'bathrooms', nbins =19)
+# c2.plotly_chart(fig, use_container_width=True)
+#
+# # Houses floor and Waterview
+# # 10 - Sidebar Max number of floors
+# f_floors = st.sidebar.selectbox('Max number of floor',
+#                                    sorted( set(data['floors'].unique())))
+#
+# # 11 - Sidebar filter water view
+# f_waterview = st.sidebar.checkbox( 'Only Houses with Water View')
+#
+#
+# # 10 and 11 Main window:
+# c1,c2 = st.columns(2)
+#
+# c1.title('Houses per floor')
+# df = data[data['floors'] < f_floors]
+# fig = px.histogram(df, x = 'floors', nbins =19)
+# c1.plotly_chart(fig, use_container_width=True)
+#
+#
+# c2.title('Waterview')
+# if f_waterview:
+#     df = data[data['waterfront'] == 1]
+# else:
+#     df= data.copy()
+#
+#
+# fig = px.histogram(df, x='waterfront', nbins=10)
+# c2.plotly_chart(fig, use_container_width=True)
+#
+#
+#
 
