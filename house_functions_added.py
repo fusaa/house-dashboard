738175e8ef9c1cc0595c12dca34a9985ea16a013
fusaa~ <ffsabbagh@gmail.com>
2022-06-28 18:04:50 +0100

    new dashboard - maps

--- house_functions_added.py
+++ house_functions_added.py
@@ -8,90 +8,134 @@ import pandas as pd
 import geopandas
 import numpy as np
 from datetime import datetime
-
 import plotly.express as px
 
 
-def overview(data):
-    # --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-    # Data Overview
 
-    # 1.1 SIDEBAR - Data Overview filters
-    f_attributes = st.sidebar.multiselect('Enter columns', data.columns) # criar campo pra especificar filtro
-    f_zipcode = st.sidebar.multiselect('Ente ZipCode', data['zipcode'].unique() )
+def features(data):
+    # Extra features on dataset:
+    data['aux_sqft_living_price'] = data['price'] / data['sqft_living']
+    zipcode_avg_price = data[['zipcode','price','sqft_living','aux_sqft_living_price','condition','grade','yr_built']].groupby('zipcode').median().reset_index()
+    zipcode_avg_price.rename(columns = {'aux_sqft_living_price': 'median_aux_sqft_living_price', 'price':'median_price'}, inplace =True)
+    # Code zipcode mean price + misc, per region in the df:
+    new_df = zipcode_avg_price[['zipcode','median_price','median_aux_sqft_living_price']]
+    new_df = new_df.rename(columns = {'mean_price':'mean_region_price', 'median_aux_sqft_living_price':'median_region_price_sqft'})
+    data = data.merge(new_df, how = 'inner', left_on='zipcode', right_on='zipcode' )
+    data['difference_median_of_region'] = (((data['aux_sqft_living_price'] / data['median_region_price_sqft'])-1)*100)
+    return data
 
-    # 1.2 MAIN CONTENT - 1st Section Data Overview
-    st.title('Data Overview')
 
-    if (f_zipcode != [] ) & ( f_attributes != [] ):
-        data = data.loc[data['zipcode'].isin(f_zipcode), f_attributes]
-    elif (f_zipcode != [] ) & (f_attributes == []):
-        data = data.loc[data['zipcode'].isin(f_zipcode), :]
-    elif (f_zipcode == [] ) & (f_attributes != []):
-        data = data.loc[:, f_attributes]
+def main(data, geofile):
+
+    # Sidebars
+    f_zipcode = st.sidebar.multiselect('Enter ZipCode(s)', data['zipcode'].unique() )
+    if (f_zipcode != [] ):
+        data = data.loc[data['zipcode'].isin(f_zipcode)]
     else:
         data = data.copy()
 
-    st.dataframe(data.head())
+    st.sidebar.title('Map Options')  # cria subtitulo na sessao do sidebar.
 
-    # 2 MAIN CONTENT - 2nd section, build average/count table
-    df1 = data[['id', 'zipcode']].groupby('zipcode').count().reset_index()
-    df2 = data[['price', 'zipcode']].groupby('zipcode').mean().reset_index()
-    df3 = data[['sqft_living', 'zipcode']].groupby('zipcode').mean().reset_index()
-    df4 = data[['price_sqft', 'zipcode']].groupby('zipcode').mean().reset_index()
-    m1 = pd.merge(df1,df2, on='zipcode', how='inner')
-    m2 = pd.merge(m1,df3, on='zipcode', how='inner')
-    df = pd.merge(m2,df4, on='zipcode', how='inner')
+    data['date'] = pd.to_datetime( data['date']).dt.strftime('%Y-%m-%d' )
 
-    df.columns = ['Zipcode','Total Houses', 'Price','Square feet -living',
-                  'Price/sqft']
+    min_year_built = int( data['yr_built'].min() )
+    max_year_built = int( data['yr_built'].max() )
+    st.sidebar.subheader('Select Min Year Built')
+    f_min_year_built = st.sidebar.slider('Yr built', min_year_built,max_year_built,min_year_built)
+    st.sidebar.subheader('Select Max Year Built')
+    f_max_year_built = st.sidebar.slider('Yr built', min_year_built,max_year_built,max_year_built)
+    data = data.loc[(data['yr_built'] <= f_max_year_built) & (data['yr_built'] >= f_min_year_built)]
 
-    c1, c2 = st.columns(( 1,1 ))  # 2 columns
-    c1.header('Average Values - S')  # 1st column
-    c1.dataframe(df, height = 600)  # 1st column
+    min_price = int(data['price'].min())
+    max_price = int(data['price'].max())
+    st.sidebar.subheader('Minimun property price')
+    min_price = st.sidebar.slider('Min Price', min_price,max_price,min_price)
+    st.sidebar.subheader('Maximum property price')
+    max_price = st.sidebar.slider('Max Price', min_price,max_price,max_price)
+    data = data.loc[(data['price'] <= max_price) & (data['price'] >= min_price)]
 
-    # 3 MAIN CONTENT - 3rd section, descriptive stats
-    num_attributes = data.select_dtypes(include=['int64', 'float64'])
-    mean = pd.DataFrame(num_attributes.apply( np.mean ))
-    median = pd.DataFrame(num_attributes.apply( np.median ))
-    std = pd.DataFrame(num_attributes.apply( np.std ))
+    min_cond = int(data['condition'].min())
+    max_cond = int(data['condition'].max())
+    st.sidebar.subheader('Minimun Condition')
+    min_cond = st.sidebar.slider('Min Condition', min_cond,max_cond,min_cond)
+    st.sidebar.subheader('Maximum Condition')
+    max_cond = st.sidebar.slider('Min Condition', min_cond,max_cond,max_cond)
+    data = data.loc[(data['condition'] <= max_cond) & (data['condition'] >= min_cond)]
 
-    max_ = pd.DataFrame( num_attributes.apply ( np.max ))
-    min_ = pd.DataFrame( num_attributes.apply ( np.min ))
+    st.sidebar.subheader('Difference lower or higher than the mean for region')
+    agree = st.sidebar.checkbox('Activate this for median % filter on sqft prices')
+    mean_comp = st.sidebar.slider('Percentage Difference(don´t forget to check the box above):', -40, 40, 0, step = 5)
+    if agree:
+        if mean_comp > 0:
+            data = data.loc[data['difference_median_of_region'] >=  float(mean_comp)]
+        elif mean_comp < 0:
+            data = data.loc[data['difference_median_of_region'] <= float(mean_comp)]
+        else:
+            data = data.loc[data['difference_median_of_region'] == float(mean_comp)]
 
-    df1 = pd.concat([max_,min_, mean, median, std], axis = 1).reset_index()
-    df1.columns = ['Attributes', 'Max', 'Min', 'Mean', 'Median', 'Std']
+    st.markdown('# **Insights on real estate for purchase and resale - Interactive Map**, __by Fulvio__')
 
-    c2.header('Descriptive Statistics')
-    c2.dataframe( df1, height = 800)
-
-    return None
-
-
-def density(data, geofile):
-    # 4 MAIN CONTENT - 4th section, portfolio density
-    st.title('Region Overview')
+    st.markdown("""
+    ***
+    This project was carried out using data available on the
+    [kaggle](https://www.kaggle.com/datasets/harlfoxem/housesalesprediction)
+    website. The objective is to identify good deals on properties for
+    purchase with subsequent profit.
+    
+    This is the interactive map for the project, it helps to find on map houses based custom criteria. 
+    Just select the options from the left sidebar and have a go.
+         
+    Do not forget to check on the sidebar that filters properties under the specific percentage mean price difference for the region. 
+    This will allow you to view the properties following the same criteria used in the project. Or even create your own.    
+    It is possible to limit the number of regions you check by selecting specific zipcodes.
+         
+    You can also check the Jupyther notebook for the project [here](https://github.com/fusaa/house-dashboard/blob/main/0%20-%20king%20county.ipynb).
+    ***
+     """)
 
     c1, c2 = st.columns((1,1))  # Columns
 
     # 4 - 4th section, column 1
     c1.header('Portfolio Density')
 
-    df = data.sample(100)
+    # df = data.sample(100)
 
     density_map = folium.Map(location = [data['lat'].mean(),
                                          data['long'].mean()],
                              default_zoom_start=15)
 
     marker_cluster = MarkerCluster().add_to(density_map)  # start class for pinmarks
-    for name, row in df.iterrows():
-        folium.Marker( [row['lat'], row['long'] ],\
-                       popup='Sold R${0} on: {1}. Features: {2} sqft, {3} bedrooms, {4} bathrooms, year built: {5}'.format( row['price'],\
-                                         row['date'],
-                                         row['sqft_living'],
-                                         row['bedrooms'],
-                                         row['bathrooms'],
-                                         row['yr_built'] ) ).add_to( marker_cluster )
+    for name, row in data.iterrows():
+
+
+        html = 'Sold USD{0} on: {1}. <br> Features: {2} sqft,<br> {3} bedrooms,<br> {4} bathrooms,<br> {5} $/sqft region, <br> {6} $/sqft this property,  year built: <br> {7}, condition: {8} <br> Transaction ID:{9}'.format(
+            row['price'], \
+            row['date'],
+            row['sqft_living'],
+            row['bedrooms'],
+            row['bathrooms'],
+            row['median_region_price_sqft'],
+            row['aux_sqft_living_price'],
+            row['yr_built'],
+            row['condition'],
+            row['id'])
+
+
+        iframe = folium.IFrame(html, width=200, height=250)
+        popup = folium.Popup(iframe, max_width=200)
+        folium.Marker([row['lat'], row['long']], \
+                      popup=popup).add_to(marker_cluster)
+        # folium.Marker( [row['lat'], row['long'] ],\
+        #                popup='Sold USD{0} on: {1}. \n Features: \n{2} sqft, {3} bedrooms, {4} bathrooms,{5} $/sqft region, {6} $/sqft this property,  year built: {7}, condition: {8}\nTransaction ID:{9}'.format( row['price'],\
+        #                                  row['date'],
+        #                                  row['sqft_living'],
+        #                                  row['bedrooms'],
+        #                                  row['bathrooms'],
+        #                                  row['median_region_price_sqft'],
+        #                                  row['aux_sqft_living_price'],
+        #                                  row['yr_built'],
+        #                                  row['condition'],
+        #                                  row['id']) ).add_to( marker_cluster )
 
     with c1:  # with - streamlit requirement rule
         folium_static(density_map) #
@@ -101,7 +145,7 @@ def density(data, geofile):
 
     df = data[['price','zipcode']].groupby('zipcode').mean().reset_index()
     df.columns = ['ZIP', 'PRICE']
-    df = df.sample(11)  # limited for faster testing
+    # df = df.sample(11)  # limited for faster testing
 
     geofile = geofile[geofile['ZIP'].isin( df['ZIP'].tolist() )] # filtering dataset with info in sample list
 
@@ -122,128 +166,61 @@ def density(data, geofile):
     with c2:  # plt data
         folium_static(region_price_map )
 
-    return None
+    ######################
+    # TODO Readme file, write it to another file.
+    readme = """
+    ***
+    ## Region Ranking
+    In order to seek the best regions to invest it has been applied a ranking system. First the top 15 regions in terms of number of transactions were selected, followed the top 10 in price, and finally the next top 5 region in terms of condition for the properties. The condition has been considered in order to avoid potential issues for investors.
+    
+    The chosen region given the criteria was zipcode 98133 (Shoreline).
+    
+    ![image](https://user-images.githubusercontent.com/66756007/176123662-a818c606-a31c-4c23-8fa5-7ea7a0c5ceda.png)
+    
+    The region has had 494 properties transactioned during the period, has a median price per square foot of USD245,21. The median lot size is 7.305 sqft, and the mean condition 3,54. And important to mention the median price for the properties are USD375.000,00 which puts the property right in the price range of properties most transactioned.
+    
+    ## Potential Deals
+    
+    A potential strategy could be acquiring properties under the median price per square foot, and reselling them over that. Say an investor buys a house 20% under the median price, that house could be resold on the median with a 20% gain, or if the investor gets a better deal, 10% over, so that would make a profit of 30%. However it is always important to check on the peculiarities of each deal. There were 213 properties in the time period transactioned under 
+    the 20% lower mark of the median price. If an investing party were to buy all of these properties the transaction ammount would be USD84.149.567,00. While the potential sell USD109.394.437,10 on a 30% higher price. That would make a possible profit of USD25.244.870,10. Of course we are considering the bulk of the area, not considering an aggresive buy would drive prices up. However a for a small investor that would be a possible strategy.
+    
+    ## Other Regions Top 5 Regions
+    
+    Even though we have chosen a single region, the other top 5 regions could yield great deals as well. Check the summary for the other 5 top regions:
+    
+    ![image](https://user-images.githubusercontent.com/66756007/176127715-cfaccd73-929c-46c4-9c8c-7beb25c7d4ab.png)
+    
+    
+    
+    """
 
 
-def hist_info(data):
-    # 6 6th Section
 
-    #  6.1 - Sidebar
-
-    st.sidebar.title('Commercial Options')  # cria subtitulo na sessao do sidebar.
-    st.title('Commercial  Attributes')
-
-    data['date'] = pd.to_datetime( data['date']).dt.strftime('%Y-%m-%d' )
-
-    min_year_built = int( data['yr_built'].min() )
-    max_year_built = int( data['yr_built'].max() )
-
-    st.sidebar.subheader('Select Max Year Built')
-    f_year_built = st.sidebar.slider('Yr built', min_year_built,max_year_built,min_year_built)
-
-    # 6.2 - Main Content
-    st.header('Average Price vs Year Built')
-
-    df = data.loc[data['yr_built'] < f_year_built]
-    df = df[['yr_built', 'price']].groupby('yr_built').mean().reset_index()
-
-    fig = px.line(df, x='yr_built', y='price')
-    st.plotly_chart(fig, use_container_width=True)
-
-    # 7 AVG price per day
-    # 7.1  Sidebar
-    st.sidebar.header("Max. date for Average Day Price")
-
-    # 7.2 - MAIN SCREEN
-    st.header('Avg Price per day')
-
-    min_date = datetime.strptime( data['date'].min(), '%Y-%m-%d' )
-    max_date = datetime.strptime( data['date'].max(), '%Y-%m-%d' )
-    f_date = st.sidebar.slider('Date', min_date, max_date, min_date)
-
-    data['date'] = pd.to_datetime(data['date'], format='%Y-%m-%d')
-    df = data.loc[data['date'] < f_date]
-    df = df[['date', 'price']].groupby('date').mean().reset_index()
-
-    fig = px.line(df, x='date', y='price')
-    st.plotly_chart(fig, use_container_width=True)
-
-    return None
+    st.markdown(readme)
 
 
-def attr_dist(data):
-    #  Section - By Attributes
-    # 8 Sidebars Bedroom
-    st.sidebar.title('Attributes Options')
-
-    f_bedrooms = st.sidebar.selectbox('Max # bedrooms:',
-                                      sorted( set(data['bedrooms'].unique())))
-
-    # 9 Sidebar Bathroom
-    f_bathrooms = st.sidebar.selectbox('Max # bathrooms:',
-                                       sorted( set(data['bathrooms'].unique())))
-
-    #  8 and 9  Main window Bedroom and bathroom
-    c1, c2 = st.columns( 2 )
 
 
-    c1.header('Bedrooms')
-    df = data[data['bedrooms'] < f_bedrooms]
-    fig = px.histogram(df, x='bedrooms', nbins =19)
-    c1.plotly_chart(fig, use_container_width=True)
-
-    c2.header('Bathrooms')
-    df = data[data['bathrooms'] < f_bathrooms]
-    fig = px.histogram(df, x = 'bathrooms', nbins =19)
-    c2.plotly_chart(fig, use_container_width=True)
-
-    # Houses floor and Waterview
-    # 10 - Sidebar Max number of floors
-    f_floors = st.sidebar.selectbox('Max number of floor',
-                                       sorted( set(data['floors'].unique())))
-
-    # 11 - Sidebar filter water view
-    f_waterview = st.sidebar.checkbox( 'Only Houses with Water View')
+@st.cache(allow_output_mutation=True)  # decorador que carrega do cache o arquivo )
+def load_data(path):
+    data = pd.read_csv(path)
+    return data
 
 
-    # 10 and 11 Main window:
-    c1,c2 = st.columns(2)
+@st.cache(allow_output_mutation=True)  # definir quadrantes pra fzer densidade por preco
+def load_geofile(url):
+    geofile = geopandas.read_file(url)
+    return geofile
 
-    c1.title('Houses per floor')
-    df = data[data['floors'] < f_floors]
-    fig = px.histogram(df, x = 'floors', nbins =19)
-    c1.plotly_chart(fig, use_container_width=True)
-
-
-    c2.title('Waterview')
-    if f_waterview:
-        df = data[data['waterfront'] == 1]
-    else:
-        df= data.copy()
-
-
-    fig = px.histogram(df, x='waterfront', nbins=10)
-    c2.plotly_chart(fig, use_container_width=True)
 
 
 if __name__ == '__main__':
-    #settings:
-    st.set_page_config( layout='wide')
+    st.set_page_config(layout='wide')
     path = './datasets/kc_house_data.csv'
-    data = pd.read_csv(path)
-
     url_geofile = 'https://opendata.arcgis.com/datasets/83fc2e72903343aabff6de8cb445b81c_2.geojson'
-    geofile = geopandas.read_file(url_geofile)
 
-    # Extra features on dataset:
-    # Adding price per squarefoot
-    data['price_sqft'] = data['price'] / data['sqft_lot']
-    # --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    data = load_data(path)
+    geofile = load_geofile(url_geofile)
 
-    overview(data)
-    density(data,geofile)
-    hist_info(data)
-    attr_dist(data)
-
-
-
+    data = features(data)
+    main(data, geofile)
